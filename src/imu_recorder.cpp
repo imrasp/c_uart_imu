a4b78c0e9500e780eecc3a90578ce0d5ebca7426
imrasp <games.rasp@gmail.com>
2018-05-05 21:09:05 +0700

    add new version of mavlink

--- src/imu_recorder.cpp
+++ src/imu_recorder.cpp
@@ -147,7 +147,7 @@ pthread_mutex_unlock(&autopilot_interface->mutexIMU);
                            << autopilot_interface->queueOdometry.front().x << sep
                            << autopilot_interface->queueOdometry.front().y << sep
                            << autopilot_interface->queueOdometry.front().z << sep
-                           << autopilot_interface->queueOdometry.front().q(0) << sep
+                           << autopilot_interface->queueOdometry.front().q[0] << sep
                            << autopilot_interface->queueOdometry.front().q(1) << sep
                            << autopilot_interface->queueOdometry.front().q(2) << sep
                            << autopilot_interface->queueOdometry.front().q(3) << endl;
