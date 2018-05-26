8b8433f4d99bacfcaf0d078825a9ef28cc85461c
imrasp <games.rasp@gmail.com>
2018-05-27 00:34:38 +0700

    stop dynamic time ref

--- src/autopilot_interface.cpp
+++ src/autopilot_interface.cpp
@@ -1164,8 +1164,9 @@ void Autopilot_Interface::set_unixtimereference(mavlink_system_time_t time){
 
     cout << "set unix time reference \n";
     // set dynamic offset an ignore time drift in system_time message
-    uint64_t ns = boost::lexical_cast<uint64_t>(std::chrono::duration_cast<std::chrono::nanoseconds>(
-            std::chrono::system_clock::now().time_since_epoch()).count());
+    uint64_t ns = boost::lexical_cast<uint64_t>(
+            std::chrono::duration_cast<std::chrono::nanoseconds>(
+                    std::chrono::system_clock::now().time_since_epoch()).count());
     if (ns - time.time_unix_usec * 1000 < 1e12) { // 1 s = 1e9 ns
         odroid_unix_ns_ref = ns;
         time_boot_ms_ref = time.time_boot_ms * 1000; //milliseconds to microsec.
@@ -1177,7 +1178,9 @@ void Autopilot_Interface::set_unixtimereference(mavlink_system_time_t time){
 
 uint64_t Autopilot_Interface::get_unixtimereference(uint32_t time){
     if(b_unixtimereference){
-        return (odroid_unix_ns_ref + ((time - time_boot_ms_ref) * 1e3));
+        uint64_t a = odroid_unix_ns_ref + ((time - time_boot_ms_ref) * 1e3);
+        cout << a <<endl;
+        return (a);
     }
     else
         return 1;
